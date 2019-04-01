769910233cb96941dbbcc174fc537db34d8b6f46
shandika <36485686+shandika@users.noreply.github.com>
2019-04-01 14:39:52 +0700

    Create template_user.php

---
+++ application/views/layout/template_user.php
@@ -0,0 +1,142 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <!-- Meta, title, CSS, favicons, etc. -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="<?= base_url('public/images/favicon.ico'); ?>" type="image/ico" />
+
+    <title>Data | Job</title>
+    <script language='JavaScript'>
+        var txt="<?php echo $title.' - '.$subtitle;?> | ";
+            var speed=250;
+            var refresh=null;
+            function action() { document.title=txt;
+            txt=txt.substring(1,txt.length)+txt.charAt(0);
+            refresh=setTimeout("action()",speed);}action();
+    </script>
+
+    <!-- Bootstrap -->
+    <link href="<?php echo base_url('assets/vendors/bootstrap/dist/css/bootstrap.min.css'); ?>" rel="stylesheet" type="text/css">
+    <!-- Font Awesome -->
+    <link href="<?php echo base_url('assets/vendors/font-awesome/css/font-awesome.min.css'); ?>" rel="stylesheet" type="text/css">
+    <!-- NProgress -->
+    <link href="<?php echo base_url('assets/vendors/nprogress/nprogress.css'); ?>" rel="stylesheet" type="text/css">
+    <!-- iCheck -->
+    <link href="<?php echo base_url('assets/vendors/iCheck/skins/flat/green.css'); ?>" rel="stylesheet" type="text/css">
+    
+    <!-- Datatables -->
+    <link href="<?php echo base_url('assets/vendors/datatables.net-bs/css/dataTables.bootstrap.min.css'); ?>" rel="stylesheet" type="text/css">
+    <link href="<?php echo base_url('assets/vendors/datatables.net-buttons-bs/css/buttons.bootstrap.min.css'); ?>" rel="stylesheet" type="text/css">
+    <link href="<?php echo base_url('assets/vendors/datatables.net-fixedheader-bs/css/fixedHeader.bootstrap.min.css'); ?>" rel="stylesheet" type="text/css">
+    <link href="<?php echo base_url('assets/vendors/datatables.net-responsive-bs/css/responsive.bootstrap.min.css'); ?>" rel="stylesheet" type="text/css">
+    <link href="<?php echo base_url('assets/vendors/datatables.net-scroller-bs/css/scroller.bootstrap.min.css'); ?>" rel="stylesheet" type="text/css">
+
+    <!-- bootstrap-progressbar -->
+    <link href="<?php echo base_url('assets/vendors/bootstrap-progressbar/css/bootstrap-progressbar-3.3.4.min.css'); ?>" rel="stylesheet" type="text/css">
+    <!-- JQVMap -->
+    <link href="<?php echo base_url('assets/vendors/jqvmap/dist/jqvmap.min.css'); ?>" rel="stylesheet" type="text/css">
+    <!-- bootstrap-daterangepicker -->
+    <link href="<?php echo base_url('assets/vendors/bootstrap-daterangepicker/daterangepicker.css'); ?>" rel="stylesheet" type="text/css">
+
+    <!-- Custom Theme Style -->
+    <link href="<?php echo base_url('assets/build/css/custom.min.css'); ?>" rel="stylesheet" type="text/css">
+  </head>
+
+   <body class="login">
+    <div class="row">
+      <?php $this->load->view($view_isi);?>
+    </div>
+    <div>
+      <div class="clearfix"></div>
+      <br />
+        <div>
+          <p><center>©<?php echo date('Y'); ?> PT. Industri Telekomunikasi Indonesia</center></p>
+        </div>
+    </div>
+
+    <!-- jQuery -->
+    <script src="<?php echo base_url('assets/vendors/jquery/dist/jquery.min.js'); ?>"></script>
+    <script src="<?php echo base_url('assets/vendors/jquery/dist/autocomplete.js'); ?>"></script>
+    <!-- Bootstrap -->
+    <script src="<?php echo base_url('assets/vendors/bootstrap/dist/js/bootstrap.min.js'); ?>"></script>
+    <!-- FastClick -->
+    <script src="<?php echo base_url('assets/vendors/fastclick/lib/fastclick.js'); ?>"></script>
+    <!-- NProgress -->
+    <script src="<?php echo base_url('assets/vendors/nprogress/nprogress.js'); ?>"></script>
+    <!-- Chart.js -->
+    <script src="<?php echo base_url('assets/vendors/Chart.js/dist/Chart.min.js'); ?>"></script>
+    <!-- gauge.js -->
+    <script src="<?php echo base_url('assets/vendors/gauge.js/dist/gauge.min.js'); ?>"></script>
+    <!-- bootstrap-progressbar -->
+    <script src="<?php echo base_url('assets/vendors/bootstrap-progressbar/bootstrap-progressbar.min.js'); ?>"></script>
+    <!-- iCheck -->
+    <script src="<?php echo base_url('assets/vendors/iCheck/icheck.min.js'); ?>"></script>
+    <!-- Skycons -->
+    <script src="<?php echo base_url('assets/vendors/skycons/skycons.js'); ?>"></script>
+    <!-- Flot -->
+    <script src="<?php echo base_url('assets/vendors/Flot/jquery.flot.js'); ?>"></script>
+    <script src="<?php echo base_url('assets/vendors/Flot/jquery.flot.pie.js'); ?>"></script>
+    <script src="<?php echo base_url('assets/vendors/Flot/jquery.flot.time.js'); ?>"></script>
+    <script src="<?php echo base_url('assets/vendors/Flot/jquery.flot.stack.js'); ?>"></script>
+    <script src="<?php echo base_url('assets/vendors/Flot/jquery.flot.resize.js'); ?>"></script>
+    <!-- Flot plugins -->
+    <script src="<?php echo base_url('assets/vendors/flot.orderbars/js/jquery.flot.orderBars.js'); ?>"></script>
+    <script src="<?php echo base_url('assets/vendors/flot-spline/js/jquery.flot.spline.min.js'); ?>"></script>
+    <script src="<?php echo base_url('assets/vendors/flot.curvedlines/curvedLines.js'); ?>"></script>
+    <!-- DateJS -->
+    <script src="<?php echo base_url('assets/vendors/DateJS/build/date.js'); ?>"></script>
+    <!-- JQVMap -->
+    <script src="<?php echo base_url('assets/vendors/jqvmap/dist/jquery.vmap.js'); ?>"></script>
+    <script src="<?php echo base_url('assets/vendors/jqvmap/dist/maps/jquery.vmap.world.js'); ?>"></script>
+    <script src="<?php echo base_url('assets/vendors/jqvmap/examples/js/jquery.vmap.sampledata.js'); ?>"></script>
+    <!-- bootstrap-daterangepicker -->
+    <script src="<?php echo base_url('assets/vendors/moment/min/moment.min.js'); ?>"></script>
+    <script src="<?php echo base_url('assets/vendors/bootstrap-daterangepicker/daterangepicker.js'); ?>"></script>
+
+    <!-- Custom Theme Scripts -->
+    <script src="<?php echo base_url('assets/build/js/custom.min.js'); ?>"></script>
+    
+    <!-- Data Tables -->
+    <script src="<?php echo base_url('assets/vendors/datatables.net/js/jquery.dataTables.min.js'); ?>"></script>
+    <script src="<?php echo base_url('assets/vendors/datatables.net-bs/js/dataTables.bootstrap.min.js'); ?>"></script>
+    <script src="<?php echo base_url('assets/vendors/datatables.net-buttons/js/dataTables.buttons.min.js'); ?>"></script>
+    <script src="<?php echo base_url('assets/vendors/datatables.net-buttons-bs/js/buttons.bootstrap.min.js'); ?>"></script>
+    <script src="<?php echo base_url('assets/vendors/datatables.net-buttons/js/buttons.flash.min.js'); ?>"></script>
+    <script src="<?php echo base_url('assets/vendors/datatables.net-buttons/js/buttons.html5.min.js'); ?>"></script>
+    <script src="<?php echo base_url('assets/vendors/datatables.net-buttons/js/buttons.print.min.js'); ?>"></script>
+    <script src="<?php echo base_url('assets/vendors/datatables.net-fixedheader/js/dataTables.fixedHeader.min.js'); ?>"></script>
+    <script src="<?php echo base_url('assets/vendors/datatables.net-keytable/js/dataTables.keyTable.min.js'); ?>"></script>
+    <script src="<?php echo base_url('assets/vendors/datatables.net-responsive/js/dataTables.responsive.min.js'); ?>"></script>
+    <script src="<?php echo base_url('assets/vendors/datatables.net-responsive-bs/js/responsive.bootstrap.js'); ?>"></script>
+    <script src="<?php echo base_url('assets/vendors/datatables.net-scroller/js/dataTables.scroller.min.js'); ?>"></script>
+    <script src="<?php echo base_url('assets/vendors/jszip/dist/jszip.min.js'); ?>"></script>
+    <script src="<?php echo base_url('assets/vendors/pdfmake/build/pdfmake.min.js'); ?>"></script>
+    <script src="<?php echo base_url('assets/vendors/pdfmake/build/vfs_fonts.js'); ?>"></script>
+
+    <!-- Chart.js -->
+    <script src="<?php echo base_url('assets/plugins/chartjs/Chart.min.js'); ?>"></script>
+    <script src="<?php echo base_url('assets/custom.js'); ?>"></script>
+
+    <!-- Sweetalert -->
+    <script src="<?php echo base_url().'assets/plugins/sweetalerts/sweetalert2.all.min.js'?>"></script>
+    <script src="<?php echo base_url().'assets/plugins/sweetalerts/myscript.js'?>"></script>
+    
+    <script type="text/javascript">
+          $(document).ready(function(){
+            
+            $('#tabeldata2').dataTable( {
+              "aaSorting": [[ 4, "desc" ]]
+              } );
+              $('#tabelcatatan2').dataTable( {
+              "aaSorting": [[ 0, "desc" ]]
+              } );
+            
+      });
+      </script>
+  
+  </body>
+</html>
