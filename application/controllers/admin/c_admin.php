42a40d23cd2e0d98811d977509ef8976055225c8
shandika <36485686+shandika@users.noreply.github.com>
2019-04-08 14:22:28 +0700

    Update c_admin.php

--- application/controllers/admin/c_admin.php
+++ application/controllers/admin/c_admin.php
@@ -13,6 +13,7 @@ class C_admin extends CI_Controller {
         $this->load->helper('form');
         $this->load->library('form_validation'); 
         $this->load->model('Model_admin','m_admin');
+        $this->load->model('Model_user','m_user');
         $this->load->library('excel'); 
         $this->load->library('pdf'); 
         $this->load->library('upload'); 
@@ -71,9 +72,154 @@ class C_admin extends CI_Controller {
         $data=$this->m_admin->GetRow($keyword);        
         echo json_encode($data);
     }
+
+    public function filter_tahun_uk(){
+        $tahun=$this->input->get('tahun');
+        //unit kerja corporate finance
+		$data['dc_januari'] = $this->m_admin->get_unit_dc_januari($tahun);
+		$data['dc_februari'] = $this->m_admin->get_unit_dc_februari($tahun);
+		$data['dc_maret'] = $this->m_admin->get_unit_dc_maret($tahun);
+		$data['dc_april'] = $this->m_admin->get_unit_dc_april($tahun);
+		$data['dc_mei'] = $this->m_admin->get_unit_dc_mei($tahun);
+		$data['dc_juni'] = $this->m_admin->get_unit_dc_juni($tahun);
+		$data['dc_juli'] = $this->m_admin->get_unit_dc_juli($tahun);
+		$data['dc_agustus'] = $this->m_admin->get_unit_dc_agustus($tahun);
+		$data['dc_september'] = $this->m_admin->get_unit_dc_september($tahun);
+		$data['dc_oktober'] = $this->m_admin->get_unit_dc_oktober($tahun);
+		$data['dc_november'] = $this->m_admin->get_unit_dc_november($tahun);
+		$data['dc_desember'] = $this->m_admin->get_unit_dc_desember($tahun);
+		//unit kerja HCMQ
+		$data['hcmq_januari'] = $this->m_admin->get_unit_hcmq_januari($tahun);
+		$data['hcmq_februari'] = $this->m_admin->get_unit_hcmq_februari($tahun);
+		$data['hcmq_maret'] = $this->m_admin->get_unit_hcmq_maret($tahun);
+		$data['hcmq_april'] = $this->m_admin->get_unit_hcmq_april($tahun);
+		$data['hcmq_mei'] = $this->m_admin->get_unit_hcmq_mei($tahun);
+		$data['hcmq_juni'] = $this->m_admin->get_unit_hcmq_juni($tahun);
+		$data['hcmq_juli'] = $this->m_admin->get_unit_hcmq_juli($tahun);
+		$data['hcmq_agustus'] = $this->m_admin->get_unit_hcmq_agustus($tahun);
+		$data['hcmq_september'] = $this->m_admin->get_unit_hcmq_september($tahun);
+		$data['hcmq_oktober'] = $this->m_admin->get_unit_hcmq_oktober($tahun);
+		$data['hcmq_november'] = $this->m_admin->get_unit_hcmq_november($tahun);
+        $data['hcmq_desember'] = $this->m_admin->get_unit_hcmq_desember($tahun);
+        //unit kerja IT & Umum
+		$data['it_januari'] = $this->m_admin->get_unit_it_januari($tahun);
+		$data['it_februari'] = $this->m_admin->get_unit_it_februari($tahun);
+		$data['it_maret'] = $this->m_admin->get_unit_it_maret($tahun);
+		$data['it_april'] = $this->m_admin->get_unit_it_april($tahun);
+		$data['it_mei'] = $this->m_admin->get_unit_it_mei($tahun);
+		$data['it_juni'] = $this->m_admin->get_unit_it_juni($tahun);
+		$data['it_juli'] = $this->m_admin->get_unit_it_juli($tahun);
+		$data['it_agustus'] = $this->m_admin->get_unit_it_agustus($tahun);
+		$data['it_september'] = $this->m_admin->get_unit_it_september($tahun);
+		$data['it_oktober'] = $this->m_admin->get_unit_it_oktober($tahun);
+		$data['it_november'] = $this->m_admin->get_unit_it_november($tahun);
+		$data['it_desember'] = $this->m_admin->get_unit_it_desember($tahun);
+        //Pengembangan Bisnis dan Produksi
+        $data['pbp_januari'] = $this->m_admin->get_unit_pbp_januari($tahun);
+        $data['pbp_februari'] = $this->m_admin->get_unit_pbp_februari($tahun);
+        $data['pbp_maret'] = $this->m_admin->get_unit_pbp_maret($tahun);
+        $data['pbp_april'] = $this->m_admin->get_unit_pbp_april($tahun);
+        $data['pbp_mei'] = $this->m_admin->get_unit_pbp_mei($tahun);
+        $data['pbp_juni'] = $this->m_admin->get_unit_pbp_juni($tahun);
+        $data['pbp_juli'] = $this->m_admin->get_unit_pbp_juli($tahun);
+        $data['pbp_agustus'] = $this->m_admin->get_unit_pbp_agustus($tahun);
+        $data['pbp_september'] = $this->m_admin->get_unit_pbp_september($tahun);
+        $data['pbp_oktober'] = $this->m_admin->get_unit_pbp_oktober($tahun);
+        $data['pbp_november'] = $this->m_admin->get_unit_pbp_november($tahun);
+        $data['pbp_desember'] = $this->m_admin->get_unit_pbp_desember($tahun);
+        //Produksi
+        $data['pd_januari'] = $this->m_admin->get_unit_pd_januari($tahun);
+        $data['pd_februari'] = $this->m_admin->get_unit_pd_februari($tahun);
+        $data['pd_maret'] = $this->m_admin->get_unit_pd_maret($tahun);
+        $data['pd_april'] = $this->m_admin->get_unit_pd_april($tahun);
+        $data['pd_mei'] = $this->m_admin->get_unit_pd_mei($tahun);
+        $data['pd_juni'] = $this->m_admin->get_unit_pd_juni($tahun);
+        $data['pd_juli'] = $this->m_admin->get_unit_pd_juli($tahun);
+        $data['pd_agustus'] = $this->m_admin->get_unit_pd_agustus($tahun);
+        $data['pd_september'] = $this->m_admin->get_unit_pd_september($tahun);
+        $data['pd_oktober'] = $this->m_admin->get_unit_pd_oktober($tahun);
+        $data['pd_november'] = $this->m_admin->get_unit_pd_november($tahun);
+        $data['pd_desember'] = $this->m_admin->get_unit_pd_desember($tahun);
+        //Satuan Pengawasan Intern
+        $data['spi_januari'] = $this->m_admin->get_unit_spi_januari($tahun);
+        $data['spi_februari'] = $this->m_admin->get_unit_spi_februari($tahun);
+        $data['spi_maret'] = $this->m_admin->get_unit_spi_maret($tahun);
+        $data['spi_april'] = $this->m_admin->get_unit_spi_april($tahun);
+        $data['spi_mei'] = $this->m_admin->get_unit_spi_mei($tahun);
+        $data['spi_juni'] = $this->m_admin->get_unit_spi_juni($tahun);
+        $data['spi_juli'] = $this->m_admin->get_unit_spi_juli($tahun);
+        $data['spi_agustus'] = $this->m_admin->get_unit_spi_agustus($tahun);
+        $data['spi_september'] = $this->m_admin->get_unit_spi_september($tahun);
+        $data['spi_oktober'] = $this->m_admin->get_unit_spi_oktober($tahun);
+        $data['spi_november'] = $this->m_admin->get_unit_spi_november($tahun);
+        $data['spi_desember'] = $this->m_admin->get_unit_spi_desember($tahun);
+        //SBU Broadband
+        $data['sbub_januari'] = $this->m_admin->get_unit_sbub_januari($tahun);
+        $data['sbub_februari'] = $this->m_admin->get_unit_sbub_februari($tahun);
+        $data['sbub_maret'] = $this->m_admin->get_unit_sbub_maret($tahun);
+        $data['sbub_april'] = $this->m_admin->get_unit_sbub_april($tahun);
+        $data['sbub_mei'] = $this->m_admin->get_unit_sbub_mei($tahun);
+        $data['sbub_juni'] = $this->m_admin->get_unit_sbub_juni($tahun);
+        $data['sbub_juli'] = $this->m_admin->get_unit_sbub_juli($tahun);
+        $data['sbub_agustus'] = $this->m_admin->get_unit_sbub_agustus($tahun);
+        $data['sbub_september'] = $this->m_admin->get_unit_sbub_september($tahun);
+        $data['sbub_oktober'] = $this->m_admin->get_unit_sbub_oktober($tahun);
+        $data['sbub_november'] = $this->m_admin->get_unit_sbub_november($tahun);
+        $data['sbub_desember'] = $this->m_admin->get_unit_sbub_desember($tahun);
+        //SBU Defense & Digital Service
+        $data['sbud_januari'] = $this->m_admin->get_unit_sbud_januari($tahun);
+        $data['sbud_februari'] = $this->m_admin->get_unit_sbud_februari($tahun);
+        $data['sbud_maret'] = $this->m_admin->get_unit_sbud_maret($tahun);
+        $data['sbud_april'] = $this->m_admin->get_unit_sbud_april($tahun);
+        $data['sbud_mei'] = $this->m_admin->get_unit_sbud_mei($tahun);
+        $data['sbud_juni'] = $this->m_admin->get_unit_sbud_juni($tahun);
+        $data['sbud_juli'] = $this->m_admin->get_unit_sbud_juli($tahun);
+        $data['sbud_agustus'] = $this->m_admin->get_unit_sbud_agustus($tahun);
+        $data['sbud_september'] = $this->m_admin->get_unit_sbud_september($tahun);
+        $data['sbud_oktober'] = $this->m_admin->get_unit_sbud_oktober($tahun);
+        $data['sbud_november'] = $this->m_admin->get_unit_sbud_november($tahun);
+        $data['sbud_desember'] = $this->m_admin->get_unit_sbud_desember($tahun);
+        //SBU Smart Energy
+        $data['sbus_januari'] = $this->m_admin->get_unit_sbus_januari($tahun);
+        $data['sbus_februari'] = $this->m_admin->get_unit_sbus_februari($tahun);
+        $data['sbus_maret'] = $this->m_admin->get_unit_sbus_maret($tahun);
+        $data['sbus_april'] = $this->m_admin->get_unit_sbus_april($tahun);
+        $data['sbus_mei'] = $this->m_admin->get_unit_sbus_mei($tahun);
+        $data['sbus_juni'] = $this->m_admin->get_unit_sbus_juni($tahun);
+        $data['sbus_juli'] = $this->m_admin->get_unit_sbus_juli($tahun);
+        $data['sbus_agustus'] = $this->m_admin->get_unit_sbus_agustus($tahun);
+        $data['sbus_september'] = $this->m_admin->get_unit_sbus_september($tahun);
+        $data['sbus_oktober'] = $this->m_admin->get_unit_sbus_oktober($tahun);
+        $data['sbus_november'] = $this->m_admin->get_unit_sbus_november($tahun);
+        $data['sbus_desember'] = $this->m_admin->get_unit_sbus_desember($tahun);
+        //Sekretaris Perusahaan
+        $data['sp_januari'] = $this->m_admin->get_unit_sp_januari($tahun);
+        $data['sp_februari'] = $this->m_admin->get_unit_sp_februari($tahun);
+        $data['sp_maret'] = $this->m_admin->get_unit_sp_maret($tahun);
+        $data['sp_april'] = $this->m_admin->get_unit_sp_april($tahun);
+        $data['sp_mei'] = $this->m_admin->get_unit_sp_mei($tahun);
+        $data['sp_juni'] = $this->m_admin->get_unit_sp_juni($tahun);
+        $data['sp_juli'] = $this->m_admin->get_unit_sp_juli($tahun);
+        $data['sp_agustus'] = $this->m_admin->get_unit_sp_agustus($tahun);
+        $data['sp_september'] = $this->m_admin->get_unit_sp_september($tahun);
+        $data['sp_oktober'] = $this->m_admin->get_unit_sp_oktober($tahun);
+        $data['sp_november'] = $this->m_admin->get_unit_sp_november($tahun);
+        $data['sp_desember'] = $this->m_admin->get_unit_sp_desember($tahun);
+
+		$data['unit']=$this->m_user->get_unit_kerja();
+		$data['data']=$this->m_admin->get_all_dokumen();
+		$data['title'] = "Dashboard";
+        $level = $this->session->userdata('level');
+        if ($level == "admin") {
+        	$data['subtitle'] = "Admin";
+        } else{
+        	$data['subtitle'] = "Staff";
+    	}
+        $data['view_isi'] = "back_end/dashboard";
+        $this->load->view('layout/template', $data);
+    }
   
     function kirim_job(){
-        
         $kodevoucher=strip_tags($this->input->post('kodevoucher'));
         $catatanadmin=strip_tags($this->input->post('catatanadmin'));
         $namastaff=strip_tags($this->input->post('country'));
