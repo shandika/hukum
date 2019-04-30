9c982cea71d6b4bce4b83bedb8fdf54b811daf95
shandika <36485686+shandika@users.noreply.github.com>
2019-04-30 07:36:06 +0700

    Update data_job_detail.php

--- application/views/data_job_detail.php
+++ application/views/data_job_detail.php
@@ -7,6 +7,35 @@
 <input type="hidden" name="stat" value="<?php echo $b['kode_voucher'];?>">
 
 <div class="col-md-12 col-xs-12">
+  <div class="x_panel">
+    <div class="x_title" id="timer">
+    <?php
+      $mulai = strtotime($b['tgl_mulai_kerja']);
+      $terlambat = strtotime($b['tgl_selesai_kerja']);
+      $now = time();
+        if($mulai == null) : 
+    ?>
+      <div class="callout callout-success">
+      <h4 align="center">Dokumen Berada di Admin.</h4>
+      <h4 align="center">Belum Ditanggapi.</h4>
+      <br>
+      
+      </div>
+      <?php elseif( $terlambat > $now ) : ?>
+        <div class="callout callout-danger">
+          <h1 align="center"><i class="fa fa-clock-o"></i> <strong class="countdown" data-time="<?=date('Y-m-d H:i:s', strtotime($b['tgl_selesai_kerja']))?>">00 Hari, 00 Jam, 00 Menit, 00 Detik</strong></h1><br/>
+          <h5 align="center">Batas Waktu Pengerjaan.</h5>
+        </div>
+      <?php else : ?>
+        <div class="callout callout-danger">
+          <h4 align="center" style="color:red">Batas Waktu Pengerjaan Sudah Habis.</h4>
+        </div>
+      <?php endif;?>
+    </div>
+  </div>
+</div>
+
+<div class="col-md-12 col-xs-12">
                 <div class="x_panel">
                   <div class="x_title">
                     <h2>Detail Job <small><?php echo $b['kode_voucher'];?></small></h2>
@@ -105,6 +134,78 @@
                 </div>
               </div>
 
+<!-- jQuery -->
+<script src="<?php echo base_url('assets/vendors/jquery/dist/jquery.min.js'); ?>"></script>
 <!-- Sweetalert -->
 <script src="<?php echo base_url().'assets/plugins/sweetalerts/sweetalert2.all.min.js'?>"></script>
 <script src="<?php echo base_url().'assets/plugins/sweetalerts/myscript.js'?>"></script>
+<!-- Script Timer -->
+<script src="<?=base_url()?>assets/build/js/token.js"></script>
+<script type="text/javascript">
+  
+  function sisawaktu(t){
+    var time = new Date(t);
+    var n = new Date();
+    var x = setInterval(function(){
+      var now = new Date().getTime();
+      var dis = time.getTime() - now;            
+            var h = Math.floor((dis % (1000 * 60 * 60 * 60)) / (1000 * 60 * 60));
+      var m = Math.floor((dis % (1000 * 60 * 60)) / (1000 * 60));
+      var s = Math.floor((dis % (1000 * 60)) / (1000));
+        h = ("0" + h).slice(-2);
+      m = ("0" + m).slice(-2);
+      s = ("0" + s).slice(-2);
+      var cd = h + ":" + m + ":" + s;
+      $('.sisawaktu').html(cd);
+    }, 100);
+    setTimeout(function() {
+      waktuHabis();
+    }, (time.getTime() - n.getTime()));
+  }
+
+  function countdown(t){
+    var time = new Date(t);
+    var n = new Date();
+    var x = setInterval(function(){
+      var now = new Date().getTime();
+      var dis = time.getTime() - now;
+      var d = Math.floor(dis / (1000 * 60 * 60 * 24));
+      var h = Math.floor((dis % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      var m = Math.floor((dis % (1000 * 60 * 60)) / (1000 * 60));
+      var s = Math.floor((dis % (1000 * 60)) / (1000));
+      d = ("0" + d).slice(-2);
+      h = ("0" + h).slice(-2);
+      m = ("0" + m).slice(-2);
+      s = ("0" + s).slice(-2);
+      var cd = d + " Hari, " + h + " Jam, " + m + " Menit, " + s + " Detik ";
+      $('.countdown').html(cd);
+      
+      setTimeout(function() {
+        location.reload()
+      }, dis);
+    }, 1000);
+  }
+
+  function ajaxcsrf(){
+    var csrfname = 'csrf_test_name';
+    var csrfhash = '89658958aa2f98aee6424a1a75b16647';
+    var csrf = {};
+    csrf[csrfname] = csrfhash;
+    $.ajaxSetup({
+      "data": csrf
+    });
+  }
+  
+  $(document).ready(function(){
+    setInterval(function() {
+      var date = new Date();
+      var h = date.getHours(), m = date.getMinutes(), s = date.getSeconds();
+      h = ("0" + h).slice(-2);
+      m = ("0" + m).slice(-2);
+      s = ("0" + s).slice(-2);
+
+      var time = h + ":" + m + ":" + s ;
+      $('.live-clock').html(time);
+    }, 1000);
+  });
+</script>
