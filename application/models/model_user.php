e4fecd3ac853b214ac9e66642ba035f16e43ee98
shandika <36485686+shandika@users.noreply.github.com>
2019-04-30 07:34:31 +0700

    Update model_user.php

--- application/models/model_user.php
+++ application/models/model_user.php
@@ -26,5 +26,12 @@ class Model_user extends CI_Model {
         $hsl=$this->db->query("SELECT tbl_new_job.*,DATE_FORMAT(tanggal_buat,'%d %M %Y %h%:%i%:%s') AS tgl FROM tbl_new_job WHERE status_pending='0'");
         return $hsl;
     }
-    
+    function get_pending_dokumen(){
+        $hsl=$this->db->query("SELECT tbl_new_job.*,DATE_FORMAT(tanggal_buat,'%d %M %Y %h%:%i%:%s') AS tgl FROM tbl_new_job WHERE status_pending='0'");
+        return $hsl;
+    }
+    function get_finish_dokumen(){
+        $hsl=$this->db->query("SELECT tbl_new_job.*,DATE_FORMAT(tanggal_buat,'%d %M %Y %h%:%i%:%s') AS tgl FROM tbl_new_job WHERE status_pending='1'");
+        return $hsl;
+    }
 }
