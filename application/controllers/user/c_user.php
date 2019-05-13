72587def8710a515f95978fe3b4db0bd1ba2ea9a
shandika <36485686+shandika@users.noreply.github.com>
2019-05-13 08:51:28 +0700

    Update c_user.php

--- application/controllers/user/c_user.php
+++ application/controllers/user/c_user.php
@@ -23,9 +23,9 @@ class C_user extends CI_Controller {
         $kodevoucherpending=strip_tags($this->input->post('kodevoucherpending'));
         $juduldokumen=strip_tags($this->input->post('juduldokumen'));
         $catatan=strip_tags($this->input->post('catatan'));
-        $tglselesai=date('Y-m-d h:i:s', strtotime("+3 day", strtotime(date("Y-m-d h:i:s"))));
         $tglmulaikerja=date('Y-m-d h:i:s');
-        $tglselesaikerja=date('Y-m-d h:i:s', strtotime("+3 day", strtotime(date("Y-m-d h:i:s"))));
+        $tgl=date('Y-m-d h:i:s');
+        $day = date('D');
 
 		if($job=strip_tags($this->input->post('job')) == "New Job"){
             $this->form_validation->set_rules('email','Email','required');
@@ -34,10 +34,36 @@ class C_user extends CI_Controller {
             $this->form_validation->set_rules('catatan','Catatan','required');
 
             if($this->form_validation->run() != false){
-                $this->load->model('model_user','m_user');
-                $this->m_user->simpan_job($email,$uk,$kodevoucher,$juduldokumen,$catatan,$tglselesai,$tglmulaikerja,$tglselesaikerja);
-                echo $this->session->set_flashdata('msg','success');
-                redirect('welcome');
+                
+                //Check hari
+                if($day=="Wed") {
+                    $tglselesaikerja1=date('Y-m-d h:i:s', strtotime("+5 day", strtotime(date("Y-m-d h:i:s"))));
+                    $tglselesai1=date('Y-m-d h:i:s', strtotime("+5 day", strtotime(date("Y-m-d h:i:s"))));
+                    $this->m_user->simpan_job_rabu($email,$uk,$kodevoucher,$juduldokumen,$catatan,$tglselesai1,$tglmulaikerja,$tglselesaikerja1);
+                    echo $this->session->set_flashdata('msg','success');
+                    redirect('welcome');
+                        }elseif($day=="Thu"){
+                          $tglselesaikerja2=date('Y-m-d h:i:s', strtotime("+5 day", strtotime(date("Y-m-d h:i:s"))));
+                          $tglselesai2=date('Y-m-d h:i:s', strtotime("+5 day", strtotime(date("Y-m-d h:i:s"))));  
+                          $this->m_user->simpan_job_kamis($email,$uk,$kodevoucher,$juduldokumen,$catatan,$tglselesai2,$tglmulaikerja,$tglselesaikerja2);
+                            echo $this->session->set_flashdata('msg','success');
+                            redirect('welcome');
+                        }elseif ($day=="Fri") {
+                            $tglselesaikerja3=date('Y-m-d h:i:s', strtotime("+5 day", strtotime(date("Y-m-d h:i:s"))));  
+                            $tglselesai3=date('Y-m-d h:i:s', strtotime("+5 day", strtotime(date("Y-m-d h:i:s"))));
+                            $this->m_user->simpan_job_jumat($email,$uk,$kodevoucher,$juduldokumen,$catatan,$tglselesai3,$tglmulaikerja,$tglselesaikerja3);
+                            echo $this->session->set_flashdata('msg','success');
+                            redirect('welcome');
+                        }elseif($day=="Sun" || $day=="Sat") {
+                            echo $this->session->set_flashdata('msg','error-job');
+                            redirect('dashboard');
+                        }else{
+                            $tglselesaikerja4=date('Y-m-d h:i:s', strtotime("+3 day", strtotime(date("Y-m-d h:i:s"))));  
+                            $tglselesai4=date('Y-m-d h:i:s', strtotime("+3 day", strtotime(date("Y-m-d h:i:s"))));
+                            $this->m_user->simpan_job($email,$uk,$kodevoucher,$juduldokumen,$catatan,$tglselesai4,$tglmulaikerja,$tglselesaikerja4);
+                                echo $this->session->set_flashdata('msg','success');
+                                redirect('welcome');
+                        }
             }else{
                 echo $this->session->set_flashdata('msg','validasi');
                 redirect('welcome');
@@ -51,10 +77,32 @@ class C_user extends CI_Controller {
                 $this->form_validation->set_rules('catatan','Catatan','required');
 
                 if($this->form_validation->run() != false){
-                    $this->load->model('model_user','m_user');
-                    $this->m_user->pending_job($kodevoucherpending,$catatan,$tglmulaikerja,$tglselesaikerja);
-                    echo $this->session->set_flashdata('msg','success-pending');
-                    redirect('welcome');
+                    
+                    //Check hari
+                if($day=="Wed") {
+                    $tglselesaikerja1=date('Y-m-d h:i:s', strtotime("+5 day", strtotime(date("Y-m-d h:i:s"))));
+                    $this->m_user->pending_job_rabu($kodevoucherpending,$catatan,$tglmulaikerja,$tglselesaikerja1);
+                            echo $this->session->set_flashdata('msg','success-pending');
+                            redirect('welcome');
+                        }elseif($day=="Thu"){
+                          $tglselesaikerja2=date('Y-m-d h:i:s', strtotime("+5 day", strtotime(date("Y-m-d h:i:s"))));
+                          $this->m_user->pending_job_kamis($kodevoucherpending,$catatan,$tglmulaikerja,$tglselesaikerja4);
+                            echo $this->session->set_flashdata('msg','success-pending');
+                            redirect('welcome');
+                        }elseif ($day=="Fri") {
+                            $tglselesaikerja3=date('Y-m-d h:i:s', strtotime("+5 day", strtotime(date("Y-m-d h:i:s"))));  
+                            $this->m_user->pending_job_jumat($kodevoucherpending,$catatan,$tglmulaikerja,$tglselesaikerja3);
+                            echo $this->session->set_flashdata('msg','success-pending');
+                            redirect('welcome');
+                        }elseif($day=="Sun" || $day=="Sat") {
+                            echo $this->session->set_flashdata('msg','error-job');
+                            redirect('dashboard');
+                        }else{
+                            $tglselesaikerja4=date('Y-m-d h:i:s', strtotime("+3 day", strtotime(date("Y-m-d h:i:s"))));  
+                            $this->m_user->pending_job($kodevoucherpending,$catatan,$tglmulaikerja,$tglselesaikerja4);
+                            echo $this->session->set_flashdata('msg','success-pending');
+                            redirect('welcome');
+                        }
                 } else{
                     echo $this->session->set_flashdata('msg','validasi');
                     redirect('welcome');
