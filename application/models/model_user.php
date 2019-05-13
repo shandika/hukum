34779bc49a9d14e98375f71c4019fc05899552e4
shandika <36485686+shandika@users.noreply.github.com>
2019-05-13 08:51:51 +0700

    Update model_user.php

--- application/models/model_user.php
+++ application/models/model_user.php
@@ -7,13 +7,43 @@ class Model_user extends CI_Model {
         parent::__construct();
     }
     
-    function simpan_job($email,$uk,$kodevoucher,$juduldokumen,$catatan,$tglselesai,$tglmulaikerja,$tglselesaikerja){
-        $hsl=$this->db->query("insert into tbl_new_job(kode_voucher,email,unit_kerja,judul_dokumen,catatan,tanggal_selesai,tgl_mulai_kerja,tgl_selesai_kerja) values ('$kodevoucher','$email','$uk','$juduldokumen','$catatan','$tglselesai','$tglmulaikerja','$tglselesaikerja')");
+    function simpan_job($email,$uk,$kodevoucher,$juduldokumen,$catatan,$tglselesai4,$tglmulaikerja,$tglselesaikerja4){
+        $hsl=$this->db->query("insert into tbl_new_job(kode_voucher,email,unit_kerja,judul_dokumen,catatan,tanggal_selesai,tgl_mulai_kerja,tgl_selesai_kerja) values ('$kodevoucher','$email','$uk','$juduldokumen','$catatan','$tglselesai4','$tglmulaikerja','$tglselesaikerja4')");
         $hsl.=$this->db->query("insert into tbl_catatan(kode_voucher,catatan_user) values ('$kodevoucher','$catatan')");
 		return $hsl;
 	}
-    function pending_job($kodevoucherpending,$catatan,$tglmulaikerja,$tglselesaikerja){
-        $hsl=$this->db->query("update tbl_new_job set catatan='$catatan', status='2', tgl_mulai_kerja='$tglmulaikerja', tgl_selesai_kerja='$tglselesaikerja' where kode_voucher='$kodevoucherpending'");
+    function simpan_job_rabu($email,$uk,$kodevoucher,$juduldokumen,$catatan,$tglselesai1,$tglmulaikerja,$tglselesaikerja1){
+        $hsl=$this->db->query("insert into tbl_new_job(kode_voucher,email,unit_kerja,judul_dokumen,catatan,tanggal_selesai,tgl_mulai_kerja,tgl_selesai_kerja) values ('$kodevoucher','$email','$uk','$juduldokumen','$catatan','$tglselesai1','$tglmulaikerja','$tglselesaikerja1')");
+        $hsl.=$this->db->query("insert into tbl_catatan(kode_voucher,catatan_user) values ('$kodevoucher','$catatan')");
+        return $hsl;
+    }
+    function simpan_job_kamis($email,$uk,$kodevoucher,$juduldokumen,$catatan,$tglselesai2,$tglmulaikerja,$tglselesaikerja2){
+        $hsl=$this->db->query("insert into tbl_new_job(kode_voucher,email,unit_kerja,judul_dokumen,catatan,tanggal_selesai,tgl_mulai_kerja,tgl_selesai_kerja) values ('$kodevoucher','$email','$uk','$juduldokumen','$catatan','$tglselesai2','$tglmulaikerja','$tglselesaikerja2')");
+        $hsl.=$this->db->query("insert into tbl_catatan(kode_voucher,catatan_user) values ('$kodevoucher','$catatan')");
+        return $hsl;
+    }
+    function simpan_job_jumat($email,$uk,$kodevoucher,$juduldokumen,$catatan,$tglselesai3,$tglmulaikerja,$tglselesaikerja3){
+        $hsl=$this->db->query("insert into tbl_new_job(kode_voucher,email,unit_kerja,judul_dokumen,catatan,tanggal_selesai,tgl_mulai_kerja,tgl_selesai_kerja) values ('$kodevoucher','$email','$uk','$juduldokumen','$catatan','$tglselesai3','$tglmulaikerja','$tglselesaikerja3')");
+        $hsl.=$this->db->query("insert into tbl_catatan(kode_voucher,catatan_user) values ('$kodevoucher','$catatan')");
+        return $hsl;
+    }
+    function pending_job($kodevoucherpending,$catatan,$tglmulaikerja,$tglselesaikerja4){
+        $hsl=$this->db->query("update tbl_new_job set catatan='$catatan', status='2', tgl_mulai_kerja='$tglmulaikerja', tgl_selesai_kerja='$tglselesaikerja4' where kode_voucher='$kodevoucherpending'");
+        $hsl.=$this->db->query("insert into tbl_catatan(kode_voucher,catatan_user) values ('$kodevoucherpending','$catatan')");
+        return $hsl;
+    }
+    function pending_job_rabu($kodevoucherpending,$catatan,$tglmulaikerja,$tglselesaikerja1){
+        $hsl=$this->db->query("update tbl_new_job set catatan='$catatan', status='2', tgl_mulai_kerja='$tglmulaikerja', tgl_selesai_kerja='$tglselesaikerja1' where kode_voucher='$kodevoucherpending'");
+        $hsl.=$this->db->query("insert into tbl_catatan(kode_voucher,catatan_user) values ('$kodevoucherpending','$catatan')");
+        return $hsl;
+    }
+    function pending_job_kamis($kodevoucherpending,$catatan,$tglmulaikerja,$tglselesaikerja2){
+        $hsl=$this->db->query("update tbl_new_job set catatan='$catatan', status='2', tgl_mulai_kerja='$tglmulaikerja', tgl_selesai_kerja='$tglselesaikerja2' where kode_voucher='$kodevoucherpending'");
+        $hsl.=$this->db->query("insert into tbl_catatan(kode_voucher,catatan_user) values ('$kodevoucherpending','$catatan')");
+        return $hsl;
+    }
+    function pending_job_jumat($kodevoucherpending,$catatan,$tglmulaikerja,$tglselesaikerja3){
+        $hsl=$this->db->query("update tbl_new_job set catatan='$catatan', status='2', tgl_mulai_kerja='$tglmulaikerja', tgl_selesai_kerja='$tglselesaikerja3' where kode_voucher='$kodevoucherpending'");
         $hsl.=$this->db->query("insert into tbl_catatan(kode_voucher,catatan_user) values ('$kodevoucherpending','$catatan')");
         return $hsl;
     }
